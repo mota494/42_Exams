a0ee8bdad7bb0d57b88cda3bb18c0513ab65e3d9
Mota494 <migueloureiromota@gmail.com>
2023-11-23 11:03:43 +0000

    Solved ft_strcpy

---
+++ exam02/level01/ft_putstr.c
@@ -0,0 +1,14 @@
+//Write a function that displays a string on the standard output.
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
---
+++ exam02/level01/ft_strcpy.c
@@ -0,0 +1,19 @@
+//Reproduce the behavior of the function strcpy (man strcpy).
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+
+	while(s1[i] != '\0')
+	{
+		s1[i] = '\0';
+		i++;
+	}
+	i = 0;
+	while(s2[i] != '\0')
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	return(s1);
+}
